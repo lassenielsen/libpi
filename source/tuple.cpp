59c94a86e06ccfb49304ec7774b40e14ead2e188
Lasse Nielsen <lasse.nielsen.dk@gmail.com>
2017-02-22 15:08:13 +0100

    Updated version and fixed small error

--- source/tuple.cpp
+++ source/tuple.cpp
@@ -28,10 +28,10 @@ void Tuple::ToStream(ostream &dest) const // {{{
   }
 } // }}}
 const shared_ptr<Value> &Tuple::GetValue(const Int &index) const // {{{
-{ return GetValue(mpz_get_ui(index.GetValue());
+{ return GetValue(mpz_get_ui(index.GetValue()));
 } // }}}
 shared_ptr<Value> &Tuple::GetValue(const Int &index) // {{{
-{ return GetValue(mpz_get_ui(index.GetValue());
+{ return GetValue(mpz_get_ui(index.GetValue()));
 } // }}}
 const shared_ptr<Value> &Tuple::GetValue(int index) const // {{{
 { return myValues[index];
